c4edae86ef4e8be424ef16db9bdca6ff6ece7bf4
jkveenstra1979 <jk@projectus.nl>
2026-05-20 21:37:56 +0200

    fix: proper PWA setup with real SW and manifest files

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'b23-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const rc = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, rc));
+    return res;
+  }).catch(() => caches.match('/'))));
+});
